04e5f0ad0329cd11061ab1842be4258cc0623379
tnpriyanka12 <tnpriyanka12@gmail.com>
2017-12-20 12:37:34 +1100

    Rearranged things with divs. visually looks good now

--- js/main.js
+++ js/main.js
@@ -16,6 +16,54 @@ $(document).ready(function(){
 
   let $box = $('.box');
   let symbolCount = 0;
+
+
+  //detecting click on boxes
+  $('.box').on('click', function(){
+    // is this square already occupied?
+    const symbolToInsert = gameChecks.checkSymbol();
+    $(this).html(symbolToInsert);
+    $(this).css({
+      height: '100px',
+      fontSize: '50px'
+    })
+    $(this).addClass('symbol'+symbolToInsert).off('click');
+    gameChecks.checkForMatch();
+  });
+
+
+//Object for game checks i.e, symbol checks and win checks
+let gameChecks = {
+
+  gameStatus: function(){
+    $gamestatus = $('#game-status');
+    symbolCount--;
+    $gamestatus.html(`Player ${this.checkSymbol()} WINS`);
+  },
+
+// const checkSymbol = function(){
+checkSymbol: function() {
+  let symbol = 0;
+  if(symbolCount % 2 == 0){
+      symbol =  'O';
+  } else{
+      symbol =  'X';
+  }
+  symbolCount++;
+  //debugger;
+  //If all  turns are over
+  $box = $('.box')
+  if(symbolCount == 9){
+    allTurnsFinished();
+    symbolCount= 0;
+  }
+  return symbol;
+},//checkSymbol
+
+
+
+//const checkForMatch = function(){
+checkForMatch: function(){
   let box = {
     1: '',
     2: '',
@@ -26,41 +74,7 @@ $(document).ready(function(){
     7: '',
     8: '',
     9: '',
-  };
-
-  //detecting click on boxes
-  $('.box').on('click', function(){
-    // is this square already occupied?
-    const symbolToInsert = checkSymbol();
-    $(this).html(symbolToInsert);
-    $(this).css({
-      height: '100px',
-      fontSize: '100px'
-    })
-    $(this).removeClass('box').addClass('symbol'+symbolToInsert);
-    checkForMatch();
-  });
-
-const checkSymbol = function(){
-  let symbol = 0;
-  if(symbolCount % 2 == 0){
-      symbol =  'O';
-  } else{
-      symbol =  'X';
-  }
-  symbolCount++;
-
-  //If all  turns are over
-  $box = $('.box')
-  if($box.length == 1){
-    allTurnsFinished();
-  }
-  return symbol;
-};
-
-
-const checkForMatch = function(){
-  //debugger;
+  };  //debugger;
   box[1] = $('#row11').html();
   box[2] = $('#row12').html();
   box[3] = $('#row13').html();
@@ -73,46 +87,55 @@ const checkForMatch = function(){
 
 //WIN LOGIC
 if(box[1]+box[2]+box[3] ==='OOO' || box[1]+box[2]+box[3] ==='XXX'){
-  console.log(`row1 match `);
-  $span = $('<span></span>');
-  $span.css({
-      borderLeft: '6px solid white',
-      height: '500px',
-      position: 'absolute',
-      left: '39%',
-      transform: 'rotateX(90deg)'
-  });
-  $('#game_container').prepend($span);
+  this.gameStatus();
 }
 else if(box[4]+box[5]+box[6] ==='OOO' || box[4]+box[5]+box[6] ==='XXX'){
-console.log(`row2 match`);
+  this.gameStatus();
 }
 else if(box[7]+box[8]+box[9] ==='OOO' || box[7]+box[8]+box[9] ==='XXX'){
+  this.gameStatus();
+
 console.log(`row3 match`);
 }
 else if(box[1]+box[4]+box[7] ==='OOO' || box[1]+box[4]+box[7] ==='XXX'){
+  this.gameStatus();
+
   console.log(`col1 match`);
 }
 else if(box[2]+box[5]+box[8] ==='OOO' || box[2]+box[5]+box[8] ==='XXX'){
+  this.gameStatus();
+
 console.log(`col2 match`);
 }
 else if(box[3]+box[6]+box[9] ==='OOO' || box[3]+box[6]+box[9] ==='XXX'){
+  this.gameStatus();
+
 console.log(`col3 match`);
 }
 else if(box[1]+box[5]+box[9] ==='OOO' || box[1]+box[5]+box[9] ==='XXX'){
+  this.gameStatus();
+
 console.log(`diag \\ match`);
 }
 else if(box[3]+box[5]+box[7] ==='OOO' || box[3]+box[5]+box[7] ==='XXX'){
+  this.gameStatus();
+
 console.log(`diag / match`);
 }
 
 
-
 }//checkForMatch ()
 
-
+};//gameChecks
 const allTurnsFinished = function(){
-  console.log(`GAME OVER`);
+  console.log(`ALL TURNS FIN MATCH DRAW`);
+  $gamestatus = $('#game-status');
+  $gamestatus.html(`MATCH DRAW`);
 }
+
+
+
+
+
 
 });//ready fn()
